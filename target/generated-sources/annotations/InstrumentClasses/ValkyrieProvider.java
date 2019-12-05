3dbe4a25eaf762a7cd56a6bb5cc144fd5c850ef2
tuxman <n.edmonds@topl.me>
2019-12-05 12:11:45 -0600

    Update Graalvm dependencies

---
+++ target/generated-sources/annotations/InstrumentClasses/ValkyrieProvider.java
@@ -0,0 +1,31 @@
+// CheckStyle: start generated
+package InstrumentClasses;
+
+import InstrumentClasses.Valkyrie;
+import com.oracle.truffle.api.dsl.GeneratedBy;
+import com.oracle.truffle.api.instrumentation.TruffleInstrument;
+import com.oracle.truffle.api.instrumentation.TruffleInstrument.Provider;
+import com.oracle.truffle.api.instrumentation.TruffleInstrument.Registration;
+import java.util.Arrays;
+import java.util.Collection;
+
+@GeneratedBy(Valkyrie.class)
+@Registration(id = "Valkyrie", name = "Valkyrie Instrument")
+public class ValkyrieProvider implements Provider {
+
+    @Override
+    public String getInstrumentClassName() {
+        return "InstrumentClasses.Valkyrie";
+    }
+
+    @Override
+    public TruffleInstrument create() {
+        return new Valkyrie();
+    }
+
+    @Override
+    public Collection<String> getServicesClassNames() {
+        return Arrays.asList("InstrumentClasses.ProgramController");
+    }
+
+}
