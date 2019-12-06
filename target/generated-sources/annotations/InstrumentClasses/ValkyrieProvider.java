3cc0b6ec615c708e8b2c341d44c568371611dc7a
tuxman <n.edmonds@topl.me>
2019-12-06 12:42:00 -0600

    Remove target files

--- target/generated-sources/annotations/InstrumentClasses/ValkyrieProvider.java
+++ target/generated-sources/annotations/InstrumentClasses/ValkyrieProvider.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @GeneratedBy(Valkyrie.class)
-@Registration(id = "Valkyrie", name = "Valkyrie Instrument")
+@Registration(id = "Valkyrie", name = "Valkyrie Instrument", version = "1.0.0")
 public class ValkyrieProvider implements Provider {
 
     @Override
