352ea2a6da13dbd5043c5c2e27fc3908a2c3dcd9
tuxman <n.edmonds@topl.me>
2019-12-30 13:07:33 -0600

    Refactor ValkyrieInstrument to create a ValkyrieNode from ValkyrieEventFactory

--- src/main/java/Instrument/ValkyrieInstrument.java
+++ src/main/java/Instrument/ValkyrieInstrument.java
@@ -65,178 +65,19 @@ public final class ValkyrieInstrument extends TruffleInstrument {
      */
     @Override
     protected void onCreate(final Env env) {
-
         final OptionValues options = env.getOptions();
         if(ENABLED.getValue(options)) {
             enable(env);
             env.registerService(this);
         }
-
-        //TODO refactor into enable function to only add source filter when ENABLED option is true
-        SourceSectionFilter.Builder builder = SourceSectionFilter.newBuilder();
-        SourceSectionFilter filter = builder.tagIs(StandardTags.CallTag.class).build();
-        Instrumenter instrumenter = env.getInstrumenter();
-        instrumenter.attachExecutionEventFactory(filter, new ExecutionEventNodeFactory(){
-
-            @Override
-            public ExecutionEventNode create(EventContext context) {
-                return new ExecutionEventNode() {
-                    @Override
-                    protected void onEnter(VirtualFrame frame) {
-                        try {
-//                            Object arg1 = frame.getArguments()[0];
-//                            Object arg2 = frame.getArguments()[1];
-//                            System.out.println(((DynamicObject) arg2).getShape());
-//                            System.out.println(((DynamicObject) arg2).get("functionData"));
-
-
-//                            Object functionObj = JSArguments.getFunctionObject(frame.getArguments());
-//                            String methodName = JSFunction.getCallTarget((DynamicObject) functionObj).toString();
-                            Node node = context.getInstrumentedNode();
-                            Iterator<Node> nodeIterator = node.getChildren().iterator();
-                            String methodName = nodeIterator.next().getSourceSection().getCharacters().toString();
-
-                            if (methodName.contains(".")) {
-                                String separatedNames[] = methodName.split("\\.");
-                                switch (separatedNames[0]) {
-                                    case "ValkyrieReserved":
-                                        switch (separatedNames[1]) {
-                                            case "createAssets": {
-                                                try {
-                                                    Object[] functionArguments = JSArguments.extractUserArguments(frame.getArguments());
-
-                                                    if (functionArguments.length == 6) {
-                                                        //TODO test long truncation
-                                                        String issuer = (String) functionArguments[0];
-                                                        String to = (String) functionArguments[1];
-                                                        Long amount = castObjectToLong(functionArguments[2]);
-                                                        String assetCode = (String) functionArguments[3];
-                                                        Long fee = castObjectToLong(functionArguments[4]);
-                                                        String data = (String) functionArguments[5];
-                                                        System.out.println(separatedNames[0] + " " + separatedNames[1] + " " + issuer + " " + to + " " + amount + " " + assetCode + " " + fee + " " + data);
-
-                                                        controller.createAssets(issuer, to, amount, assetCode, fee, data);
-
-                                                        //TODO return tx json like API does
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind(true);
-
-                                                    } else {
-                                                        controller.didExecuteCorrectly = false;
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind("ValkyrieError: Incorrect number of arguments to Valkyrie_createAssets");
-                                                    }
-                                                } catch (Exception e) {
-                                                    controller.didExecuteCorrectly = false;
-                                                    CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                    throw context.createUnwind("ValkyrieError: " + e.getMessage());
-                                                }
-                                            }
-
-                                            case "transferAssets": {
-                                                try {
-                                                    Object[] functionArguments = JSArguments.extractUserArguments(frame.getArguments());
-
-                                                    if (functionArguments.length == 6) {
-                                                        String issuer = (String) functionArguments[0];
-                                                        String from = (String) functionArguments[1];
-                                                        String to = (String) functionArguments[2];
-                                                        Long amount = castObjectToLong(functionArguments[3]);
-                                                        String assetCode = (String) functionArguments[4];
-                                                        Long fee = castObjectToLong(functionArguments[5]);
-                                                        System.out.println(separatedNames[0] + " " + separatedNames[1] + " " + issuer + " " + from + " " + to + " " + amount + " " + assetCode + " " + fee);
-
-                                                        controller.transferAssets(issuer, from, to, amount, assetCode, fee);
-
-                                                        //TODO return more meaningful response than boolean
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind(true);
-
-                                                    } else {
-                                                        controller.didExecuteCorrectly = false;
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind("ValkyrieError: Incorrect number of arguments to Valkyrie_transferAssets");
-                                                    }
-                                                } catch (Exception e) {
-                                                    controller.didExecuteCorrectly = false;
-                                                    CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                    throw context.createUnwind("ValkyrieError: " + e.getMessage());
-                                                }
-                                            }
-
-                                            case "transferArbits": {
-                                                try {
-                                                    Object[] functionArguments = JSArguments.extractUserArguments(frame.getArguments());
-
-                                                    if (functionArguments.length == 4) {
-                                                        String from = (String) functionArguments[0];
-                                                        String to = (String) functionArguments[1];
-                                                        Long amount = castObjectToLong(functionArguments[2]);
-                                                        Long fee = castObjectToLong(functionArguments[3]);
-                                                        System.out.println(separatedNames[0] + " " + separatedNames[1] + " " + from + " " + to + " " + amount + " " + fee);
-
-                                                        controller.transferArbits(from, to, amount, fee);
-
-                                                        //TODO return more meaningful response than boolean
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind(true);
-
-                                                    } else {
-                                                        controller.didExecuteCorrectly = false;
-                                                        CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                        throw context.createUnwind("ValkyrieError: Incorrect number of arguments to Valkyrie_transferArbits");
-                                                    }
-                                                } catch (Exception e) {
-                                                    controller.didExecuteCorrectly = false;
-                                                    CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                    throw context.createUnwind("ValkyrieError: " + e.getMessage());
-                                                }
-                                            }
-
-                                            default:
-                                                controller.didExecuteCorrectly = false;
-                                                CompilerDirectives.transferToInterpreterAndInvalidate();
-                                                throw context.createUnwind("ValkyrieError: Method not found in ValkyrieInstrument namespace");
-
-                                        }
-                                        //Non valkyrie (ordinary) function - do nothing
-                                    default:
-                                }
-                            }
-                        }
-                        catch (Exception e) {
-                            //do nothing
-                        }
-                    }
-
-
-                    @Override
-                    public void onReturnValue(VirtualFrame frame, Object result) {
-
-                    }
-
-                    @Override
-                    public void onReturnExceptional(VirtualFrame frame, Throwable exception) {
-
-                    }
-
-
-                    @Override
-                    public Object onUnwind(VirtualFrame frame, Object info) {
-                        return info;
-                    }
-
-                };
-            }
-        });
-
-        //controller = factory.create(env);
-        //env.registerService(controller);
-        //this.env = env;
     }
 
     private void enable(final Env env) {
-        //TODO move SourceSection here
+        controller = factory.create(env);
+        SourceSectionFilter.Builder builder = SourceSectionFilter.newBuilder();
+        SourceSectionFilter filter = builder.tagIs(StandardTags.CallTag.class).build();
+        Instrumenter instrumenter = env.getInstrumenter();
+        instrumenter.attachExecutionEventFactory(filter, new ValkyrieEventFactory(this, controller));
     }
 
     public static ProgramController getController(Engine engine){
@@ -245,10 +86,6 @@ public final class ValkyrieInstrument extends TruffleInstrument {
             throw new IllegalStateException("Instrument is not installed.");
         }
         return instrument.lookup(ProgramController.class);
-    }
-
-    public Env getEnv() {
-        return this.env;
     }
 
     @Override
